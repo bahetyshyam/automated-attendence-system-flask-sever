3ee41172bddee16d2afe2b7bac49f0935fc0b8e9
Kishan Kumar <itskishankumar98@gmail.com>
2020-05-06 19:50:55 +0530

    Added face-recognition API

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import os, pathlib
 from face_scrapper.face_scrapper import extract_faces
 import requests
 import shutil
+import face_recognition
 
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 
@@ -22,18 +23,16 @@ app = Flask(__name__)
 def home():
     return 'Home'
 
-@app.route('/attendence', methods=['POST'])
-def getAttendence():
+@app.route('/face-detection', methods=['POST'])
+def faceDetection():
     #check if the key value pair of the image sent is correct or not
     if 'image' not in request.files:
         return jsonify({'message': '''Key name of file sent should be "image" '''})
     file = request.files['image']
 
-
     #check if a file is selected or not
     if file.filename == '':
         return 'No selected file'
-
 
     #check if file is one of those extensions
     if file and allowed_file(file.filename):
@@ -64,6 +63,68 @@ def getAttendence():
     else:
         return "Upload png, jpg, or jpeg image only"
 
+@app.route('/face-recognition', methods=['POST'])
+def faceRecognition():
+    if 'image' not in request.files:
+        return jsonify({'message': '''Key name of file sent should be "image" '''})
+    file = request.files['image']
+
+    #check if a file is selected or not
+    if file.filename == '':
+        return 'No selected file'
+
+    #train the model with single faces
+    abhin_image = face_recognition.load_image_file("./training_set/abhin.jpg")
+    gs_image = face_recognition.load_image_file("./training_set/gs.jpg")
+    kishan_image = face_recognition.load_image_file("./training_set/kishan.jpg")
+    monisha_image = face_recognition.load_image_file("./training_set/monisha.jpg")
+    naik_image = face_recognition.load_image_file("./training_set/naik.jpg")
+    prathik_image = face_recognition.load_image_file("./training_set/prathik.jpg")
+    rachana_image = face_recognition.load_image_file("./training_set/rachana.jpg")
+    sharad_image = face_recognition.load_image_file("./training_set/sharad.jpg")
+    shyam_image = face_recognition.load_image_file("./training_set/shyam.jpg")
+    vishwa_image = face_recognition.load_image_file("./training_set/vishwa.jpg")
+    #load the new unknown posted image
+    unknown_image = face_recognition.load_image_file(file)
+
+    try:
+        #create face encodings for each face and for new unknown posted image
+        abhin_face_encoding = face_recognition.face_encodings(abhin_image)[0]
+        gs_face_encoding = face_recognition.face_encodings(gs_image)[0]
+        kishan_face_encoding = face_recognition.face_encodings(kishan_image)[0]
+        monisha_face_encoding = face_recognition.face_encodings(monisha_image)[0]
+        naik_face_encoding = face_recognition.face_encodings(naik_image)[0]
+        prathik_face_encoding = face_recognition.face_encodings(prathik_image)[0]
+        rachana_face_encoding = face_recognition.face_encodings(rachana_image)[0]
+        sharad_face_encoding = face_recognition.face_encodings(sharad_image)[0]
+        shyam_face_encoding = face_recognition.face_encodings(shyam_image)[0]
+        vishwa_face_encoding = face_recognition.face_encodings(vishwa_image)[0]
+        unknown_face_encoding = face_recognition.face_encodings(unknown_image)[0]
+    except IndexError:
+        print("I wasn't able to locate any faces in at least one of the images. Check the image files. Aborting...")
+        quit()
+
+    known_faces = [
+        abhin_face_encoding,
+        gs_face_encoding,
+        kishan_face_encoding,
+        monisha_face_encoding,
+        naik_face_encoding,
+        prathik_face_encoding,
+        rachana_face_encoding,
+        sharad_face_encoding,
+        shyam_face_encoding,
+        vishwa_face_encoding
+    ]
+
+    results = face_recognition.compare_faces(known_faces, unknown_face_encoding)
+
+    for i in range(len(results)):
+        if results[i] == True:
+            #return index of person recognized
+            return str(i)
+    #else return -1
+    return '-1'
 
 if __name__ == '__main__':
     app.run(debug=True, port= 8080)
